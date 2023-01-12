8fe9db42ca449d6137c6b1d7ced33e29172bc402
oaltay <ozgenur12331@gmail.com>
2023-01-12 21:49:48 +0300

    Initial commit

---
+++ TemplateDesignPattern.UpSchool/DAL/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TemplateDesignPattern.UpSchool.DAL.Entities;
+
+namespace TemplateDesignPattern.UpSchool.DAL
+{
+    public class Context : IdentityDbContext<AppUser,AppRole,int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=LAPTOP-8GBIAO0U\\SQLEXPRESS;Database=DbTemplate;integrated security=true");
+        }
+    }
+}
